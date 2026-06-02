7a0cf5f963db590efd1f8fcb1d5b723c3800ab07
nabilalfadila_ <nabilalfadila89@gmail.com>
2026-06-02 11:43:47 +0800

    Implement validation, error handling & service layer

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,10 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
-        'name'
+        'name',
     ];
+
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
 }
--- app/Models/Item.php
+++ app/Models/Item.php
@@ -3,13 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Item extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'name',
         'quantity',
         'price',
-        'category_id'
+        'category_id',
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
